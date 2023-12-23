c54678384e95020d9b259fee97e962235a434f1f
KyleQiao1992 <chocolatman1992@gmail.com>
2023-12-23 18:37:26 +0800

    up

--- src/main/java/com/mac/drive/macDrive/mapper/RoleMapper.java
+++
@@ -1,8 +0,0 @@
-package com.mac.drive.macDrive.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.mac.drive.macDrive.pojo.Role;
-
-public interface RoleMapper extends BaseMapper<Role> {
-
-}
